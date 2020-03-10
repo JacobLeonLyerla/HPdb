80391d91e4cc3f2d6df6dc5b24b8af8b94038f48
Jacob <jacobleonlyerla@gmail.com>
2020-03-09 21:11:13 -0700

    formatting

--- Hufflepuff/hufflepuff.js
+++ Hufflepuff/hufflepuff.js
@@ -1,49 +1,49 @@
 const mongoose = require('mongoose');
 
 const Hufflepuff = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true,
-      },
-    aliases:String,
-    age:{
-        type:Number,
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    aliases: String,
+    age: {
+        type: Number,
         defualt: "Unknown"
     },
-    DoB:String,
-    place_of_birth:String,
-    country:String,
-    hogwarts_house:{
-        type:String,
-        required:true,
+    DoB: String,
+    place_of_birth: String,
+    country: String,
+    hogwarts_house: {
+        type: String,
+        required: true,
     },
-    student:{
-        type:Boolean,
-        required:true,
+    student: {
+        type: Boolean,
+        required: true,
     },
-    graduated:{
-        type:Boolean,
-        required:true,  
+    graduated: {
+        type: Boolean,
+        required: true,
     },
-    expelled:Boolean,
-    active:String,
-    gender:String,
-    species:String,
-    height:String,
-    hair_color:String,
-    eye_color:String,
-    skin_color:String,
-    img:{
-        type:String,
-        default:"https://pre00.deviantart.net/ad55/th/pre/f/2007/158/d/8/hufflepuff_wall_by_ibuki_san.jpg"
+    expelled: Boolean,
+    active: String,
+    gender: String,
+    species: String,
+    height: String,
+    hair_color: String,
+    eye_color: String,
+    skin_color: String,
+    img: {
+        type: String,
+        default: "https://pre00.deviantart.net/ad55/th/pre/f/2007/158/d/8/hufflepuff_wall_by_ibuki_san.jpg"
     },
-    wikiaInfo:{
-        type:String,
+    wikiaInfo: {
+        type: String,
     },
-    lore:String,
+    lore: String,
 
 
 })
 
-module.exports = mongoose.model('Hufflepuff',Hufflepuff)
+module.exports = mongoose.model('Hufflepuff', Hufflepuff)
